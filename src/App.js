2b928aaf41a51f141ca61ba2e8173168c6e0a608
Ranamasud1414 <ranamasud1052002@gmail.com>
2022-10-06 20:33:20 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ToDoList  from "./TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=>{
+ const[item,setitems]=useState();
+ const[Items,setItems]=useState([]);
+
+const items=(event)=>{
+  
+      setitems(event.target.value);
+  
 }
+
+const listOfItems=()=>{
+  setItems((oldItems)=>{
+    return [...oldItems,item];
+
+    
+  });
+  setitems('');
+
+};
+
+
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br/>
+          <h1>To Do List</h1>
+          <br/>
+          <input type="text" placeholder="add items" value={item} onChange={items}/>
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+          
+         { Items.map((itemvalue)=>{  //we ccalling the empty array using map
+           return <ToDoList text={itemvalue}/>
+          })}
+          
+        </ol>
+
+        </div>
+
+        
+      </div>
+    </>
+  )
+};
 
 export default App;
